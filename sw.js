eeec4d0b944091725feee63bea4eb959d013c819
ecofincreditsouharda <ecofincreditsouharda@gmail.com>
2026-07-28 01:16:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coopfr-v13';
+const CACHE = 'coopfr-v14';
 const ASSETS = ['./', 'index.html', 'app.js', 'style.css', 'manifest.webmanifest', 'logo.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
